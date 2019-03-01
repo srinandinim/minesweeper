5a8aa705b44f0cdf9641b74624c89e27ff815316
Srinandini Marpaka <srinandinim@hotmail.com>
2019-02-28 20:26:33 -0500

    Added numbers for the buttons adjacent to mine

--- Minesweeper.java
+++ Minesweeper.java
@@ -6,6 +6,7 @@ import java.awt.event.*;
 import java.awt.image.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 import javax.imageio.ImageIO;
@@ -13,7 +14,10 @@ import javax.imageio.ImageIO;
 public class Minesweeper extends JPanel implements ActionListener, MouseListener {
 
 	/*
-	*Ensure first click is not a mine
+	* Ensure first click is not a mine
+	* Disable button when it is left clicked
+	* Expand map
+	* Timer
 	*/
 
 	JFrame frame;
@@ -28,6 +32,8 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 
 	JPanel panel, topPanel, scoreBoard;
 	JToggleButton[][] togglers;
+	int[][] numbers;
+	Color[] numberColors;
 	int width = 9;
 	int height = 9;
 	ImageIcon[] flag, mine, centerImage;
@@ -35,6 +41,8 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 	JLabel timer, flagsRemaining;
 	int startingFlag;
 	int flagCount = 0;
+	Date startDate;
+	int seconds;
 	String spacing = "     ";
 	
 	ArrayList<Point> minePositions;
@@ -43,10 +51,11 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 		frame = new JFrame("Minesweeper");
 		frame.add(this);
 		frame.setSize(40*width,40*height);
+		
+		//Center, Mine, Flag Images
 		mine = new ImageIcon[4];
 		flag = new ImageIcon[4];
 		centerImage = new ImageIcon[4];
-
 		mine[0] = new ImageIcon ("dMine.png");
 		flag[0] = new ImageIcon ("dFlag.png");
 		centerImage[0] = new ImageIcon ("dCenter.png");
@@ -60,11 +69,11 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 		flag[3] = new ImageIcon(flag[0].getImage().getScaledInstance(25,25, Image.SCALE_SMOOTH));
 		centerImage[3] = new ImageIcon(centerImage[0].getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH));
 
+		//Menu Bar
 		menuBar = new JMenuBar();
 		game = new JMenu ("Game");
 		display = new JMenu ("Icons");
 		control = new JMenu ("Control");
-
 		beginner = new JMenuItem ("Beginner");
 		intermediate = new JMenuItem ("Intermediate");
 		expert = new JMenuItem ("Expert");
@@ -76,6 +85,7 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 			item.addActionListener(this);
 			game.add(item);
 		}
+		
 		mDefault = new JMenuItem ("Default");
 		sweet = new JMenuItem ("Candyland");
 		savory = new JMenuItem ("Fair");
@@ -93,12 +103,15 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 		for (JLabel label: directions){
 			control.add(label);
 		}
-
+		
 		menuBar.add(game);
 		menuBar.add(display);
 		menuBar.add(control);
-
+		
+		//Panel Board
 		togglers = new JToggleButton[width][height];
+		numbers = new int[width][height];
+		numberColors = new Color[9];
 		panel = new JPanel();
 		panel.setLayout(new GridLayout(togglers.length, togglers[0].length));
 		for (int i = 0; i < togglers.length; i++){
@@ -111,19 +124,32 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 		minePositions = new ArrayList<>();
 		gameSetup();
 		
+		//Setting Colors for Numbers
+		numberColors[0] = Color.BLUE; //Standard Blue
+		numberColors[1] = new Color(0, 100, 0); //Dark Green
+		numberColors[2] = Color.RED; //Standard Red
+		numberColors[3] = Color.BLACK; //Standard Black
+		numberColors[4] = new Color(255, 140, 0); //Dark Orange
+		numberColors[5] = new Color(139, 69, 19); //Saddle Brown
+		numberColors[6] = new Color(128, 0, 128); //Purple
+		numberColors[7] = new Color(128, 0, 0); //Maroon
+		numberColors[8] = new Color(255, 20, 147); //Deep Pink
+		
+		//Score Board
 		scoreBoard = new JPanel(new FlowLayout(FlowLayout.CENTER));
 		startingFlag = 10;
 		flagsRemaining = new JLabel(spacing + (startingFlag-flagCount) + spacing);
-		flagsRemaining.setFont(new Font("Serif", Font.PLAIN, 24));
+		flagsRemaining.setFont(new Font("Lucida Sans", Font.PLAIN, 24));
 		center = new JButton (centerImage[3]);
 		center.setPreferredSize(new Dimension(50, 50));
 		center.addActionListener(this);
-		timer = new JLabel(spacing + "10" + spacing);
-		timer.setFont(new Font("Serif", Font.PLAIN, 24));
+		timer = new JLabel(spacing + "0" + spacing);
+		timer.setFont(new Font("Lucida Sans", Font.PLAIN, 24));
 		scoreBoard.add(flagsRemaining);
 		scoreBoard.add(center);
 		scoreBoard.add(timer);
 
+		//Combine Panels
 		topPanel = new JPanel (new BorderLayout());
 		topPanel.add(menuBar, BorderLayout.NORTH);
 		topPanel.add(scoreBoard, BorderLayout.SOUTH);
@@ -181,6 +207,13 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 		if (width == 30)
 			minesRemaining = 90;
 		
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers[0].length; j++) {
+				numbers[i][j] = 0;
+			}
+		}
+		
+		//Randomly assign the mine positions
 		minePositions = new ArrayList<>();
 		while (minesRemaining > 0){
 			int row;
@@ -189,9 +222,29 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 				row = (int)(Math.random() * height);
 				col = (int)(Math.random() * width);
 			} while (togglers[row][col].getIcon()!=null);
-			minePositions.add(new Point(row, col));			
+			minePositions.add(new Point(row, col));	
+			numbers[row][col] = -1;
 			minesRemaining--;
 		}
+
+		//Fill buttons with values based on mine positions
+		for (int i = 0; i < numbers.length; i++) {
+			for (int j = 0; j < numbers[0].length; j++) {
+				if (numbers[i][j] == -1) {
+					for (int x = -1; x < 2; x++) {
+						for (int y = -1; y < 2; y++) {
+							try {
+								if (numbers[i+x][j+y] != -1) {
+									numbers[i+x][j+y]+=1;
+								}
+							} catch (Exception e) {}
+						}
+					}
+				}
+				//togglers[i][j].setText(numbers[i][j] + "");
+			}
+		}
+		
 		System.out.println(minePositions);
 	}
 
@@ -211,11 +264,12 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 		centerImage[3] = new ImageIcon(centerImage[number].getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH));
 		center.setIcon(centerImage[3]);
 	}
-
+	
 	public void changeLevel(){
 		frame.remove(panel);
 		frame.setSize(40*width,40*height);
 		togglers = new JToggleButton[height][width];
+		numbers = new int[height][width];
 		panel = new JPanel();
 		panel.setLayout(new GridLayout(togglers.length, togglers[0].length));
 		for (int i = 0; i < togglers.length; i++){
@@ -227,6 +281,8 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 		}
 		flagCount = 0;
 		flagsRemaining.setText(spacing + (startingFlag-flagCount) + spacing);
+		seconds = 0;
+		timer.setText(spacing + seconds + spacing);
 		gameSetup();
 		frame.add(panel,BorderLayout.CENTER);
 	}
@@ -237,12 +293,21 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 				if (minePositions.contains(new Point(i, j))) {
 					togglers[i][j].setIcon (mine[3]);
 				}
+				//Fix the statement below = does not really work
 				togglers[i][j].setEnabled(false);
 			}
 		}
 	}
+	
+	public long getTimerValue() {
+		return (new Date().getTime() - startDate.getTime())/1000;
+	}
 
 	public void mousePressed (MouseEvent e){
+		if (seconds == 0) {
+			startDate = new Date();
+			timer.setText(spacing + getTimerValue() + spacing);
+		}
 		if (e.getButton() == MouseEvent.BUTTON3){
 			for (int i = 0; i < togglers.length; i++){
 				for (int j = 0; j < togglers[0].length; j++){
@@ -268,6 +333,13 @@ public class Minesweeper extends JPanel implements ActionListener, MouseListener
 						if (minePositions.contains(new Point(i, j))) {
 							togglers[i][j].setIcon (mine[3]);
 							gameOver();
+						} else {
+							if (numbers[i][j] != 0) {
+								togglers[i][j].setMargin(new Insets(0,0,0,0));
+								togglers[i][j].setFont(new Font("Lucida Sans", Font.PLAIN, 20));
+								togglers[i][j].setForeground(numberColors[numbers[i][j]-1]);
+								togglers[i][j].setText(numbers[i][j]+"");
+							}
 						}
 					}
 				}
